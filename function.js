7f9fec0db37087b57e9e891cd1c8236ebab66137
Ashutosh Mishra <69295774+enthuashu@users.noreply.github.com>
2022-07-15 16:16:18 +0530

    update

---
+++ function.js
@@ -0,0 +1,24 @@
+// function functionname
+
+// function declaration
+function manager1() {
+  console.log("The attendence of today is", 345);
+}
+
+function manager2() {
+  console.log("Cleanliss has been done ");
+}
+
+function manager3() {
+  console.log("New Employyes are hired");
+}
+
+//boss calling for attendence
+console.log("Boss calling for attendence");
+manager1(); // function call
+
+console.log("Boss calling for cleanliness");
+manager2(); // function call
+
+console.log("Boss calling for hiring");
+manager3(); // function call
